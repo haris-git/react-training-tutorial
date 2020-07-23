3b620b108a5a81c0270ff2025dadf1a103e69da3
Charis Logothetis <harislogothetis@gmail.com>
2020-07-23 12:19:56 +0300

    Use recursion to create the items of the list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import './App.css';
 
 const displayEmojiName = event => alert(event.target.id);
 
+const emojis = [
+  {
+    emoji: '😀',
+    name: "test grinning face"
+  },
+  {
+    emoji: '🎉',
+    name: "party popper"
+  },
+  {
+    emoji: '💃',
+    name: "woman dancing"
+  }
+];
+
 function App() {
   var greeting = "greeting";
 
@@ -11,21 +26,15 @@ function App() {
       <h1 id={ greeting }>Hello, World</h1>
       <p>I am writing JSX</p>
       <ul>
-        <li>
-          <button onClick={ displayEmojiName }>
-            <span role="img" aria-label="grinning face" id="grinning face">😀</span>
-          </button>
-        </li>
-        <li>
-          <button onClick={ displayEmojiName }>
-            <span role="img" aria-label="party popper" id="party popper">🎉</span>
-          </button>
-        </li>
-        <li>
-          <button onClick={ displayEmojiName }>
-            <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
-          </button>
-        </li>
+        {
+          emojis.map(emoji => (
+            <li key={ emoji.name }>
+              <button onClick={ displayEmojiName }>
+                <span role="img" aria-label={ emoji.name } id={ emoji.name }>{ emoji.emoji }</span>
+              </button>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )

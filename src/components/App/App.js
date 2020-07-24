f88a2dd532aec3d268ed1be821bfb8f2d0fbc3d8
Charis Logothetis <harislogothetis@gmail.com>
2020-07-24 09:04:18 +0300

    Include AnimalCart into App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import data from './data';
+import AnimalCard from '../AnimalCard/AnimalCard';
 import Instructions from '../Instructions/Instructions';
 import './App.css';
 
@@ -23,21 +25,30 @@ function App() {
   var greeting = "greeting";
 
   return(
-    <div className="container">
-      <h1 id={ greeting }>Hello, World</h1>
-      <Instructions />
-      <ul>
-        {
-          emojis.map(emoji => (
-            <li key={ emoji.name }>
-              <button onClick={ displayEmojiName }>
-                <span role="img" aria-label={ emoji.name } id={ emoji.name }>{ emoji.emoji }</span>
-              </button>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+    <>
+      <div className="container">
+        <h1 id={ greeting }>Hello, World</h1>
+        <Instructions />
+        <ul>
+          {
+            emojis.map(emoji => (
+              <li key={ emoji.name }>
+                <button onClick={ displayEmojiName }>
+                  <span role="img" aria-label={ emoji.name } id={ emoji.name }>{ emoji.emoji }</span>
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className="wrapper">
+        <h1>Animals</h1>
+        {data.map(animal => (
+          <AnimalCard key={animal.name}/>
+        ))}
+      </div>
+    </>
   )
 }
 

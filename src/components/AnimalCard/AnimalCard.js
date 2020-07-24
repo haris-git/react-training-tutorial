97a45b16e707c513e404989204cbcddddeb8c30e
Charis Logothetis <harislogothetis@gmail.com>
2020-07-24 09:36:05 +0300

    Add css to the AnimalCard component

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -10,7 +10,7 @@ export default function AnimalCard({
   size
 }) {
   return (
-    <div>
+    <div className="animal-wrapper">
       <h2>{name}</h2>
       <h3>{scientificName}</h3>
       <h4>{size}kg</h4>
